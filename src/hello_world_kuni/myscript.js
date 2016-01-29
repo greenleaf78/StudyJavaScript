f6e36b6f379d7faa5705436905c55b8a8a9e3cb7
kunimasa_satoshi <kunimasa_satoshi@ogis-ri.co.jp>
2016-01-29 04:02:42 +0000

    add

---
+++ src/hello_world_kuni/myscript.js
@@ -0,0 +1,10 @@
+  for(var i=0; i< 5; i++){
+    if(i%2 == 1){
+      console.log(`Hellooo, JavaScript! ${i}`);
+      console.log(i);
+    }
+    document.write(i);
+  }
+  
+  var x = document.getElementById('main');
+  console.log(x);
